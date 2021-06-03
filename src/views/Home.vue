ebe941283eb52dfe20f33fdefa41df2430cdb6f5
MikePreston17 <michael.n.preston@gmail.com>
2021-06-02 22:24:51 -0500

    added hookstate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,25 @@
 <template>
   <div class="home flex items-center">
     <ul>
+      <HookStateCounter />
       <TailwindCard />
       <ChitChatToast />
-      <!-- <Palette /> -->
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Reverser from "@/components/Reverser.vue";
-import Books from "@/components/Books.vue";
-import ListItem from "@/components/ListItem.vue";
-import InventoryList from "@/components/InventoryList.vue";
-import TodoList from "@/components/todo/TodoList.vue";
 import TailwindCard from "@/components/TailwindCard.vue";
 import ChitChatToast from "@/components/ChitChatToast.vue";
-import Palette from "@/components/Palette.vue";
+import HookStateCounter from "@/components/HookStateCounter.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    // Reverser,
-    // Books,
-    // ListItem,
-    // InventoryList,
-    // TodoList,
     TailwindCard,
     ChitChatToast,
-    // Palette,
+    HookStateCounter,
   },
 });
 </script>
