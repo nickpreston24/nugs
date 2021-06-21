8423e711e88a062d3bf037ea6b400b0f0cf0d2e5
MikePreston17 <michael.n.preston@gmail.com>
2021-06-21 02:20:42 -0500

    Started on the NugsList by adding axios and nugs-api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home flex items-center">
     <ul>
+      <NugList />
       <HookStateCounter />
       <TailwindCard />
       <ChitChatToast />
+      <!-- <Capacitor/> -->
     </ul>
   </div>
 </template>
@@ -13,13 +15,15 @@ import { defineComponent } from "vue";
 import TailwindCard from "@/components/TailwindCard.vue";
 import ChitChatToast from "@/components/ChitChatToast.vue";
 import HookStateCounter from "@/components/HookStateCounter.vue";
-
+import Capacitor from "@/components/Capacitor.vue";
+import NugList from "../components/nugs/NugList.vue";
 export default defineComponent({
   name: "Home",
   components: {
     TailwindCard,
     ChitChatToast,
     HookStateCounter,
+    NugList,
   },
 });
 </script>
