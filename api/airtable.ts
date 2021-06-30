e78be10871ae64853462ce70c7ea879d1f59c793
MikePreston17 <michael.n.preston@gmail.com>
2021-06-30 04:29:43 -0500

    Bound form; Airtable instance needs fixed

--- api/airtable.ts
+++ api/airtable.ts
@@ -1,9 +1,39 @@
 import Airtable = require('airtable')
 
-type Order = {
+export type Order = {
     Cart: [],
     "Buy?": boolean,
     "Count Desired": 0,
+}
+
+export type Round = {
+    Name: "",
+    Grain: 0,
+    "Muzzle Velocity": 0,
+    "Found": "",
+    "Caliber": "",
+    "Manufacturer": "",
+    "Diameter": 0.0
+}
+
+export const createRound = async (rounds: Round[]) => {
+
+    const base = new Airtable({ apiKey: "keyl5Wo5ETa4HR4tt" }).base(
+        "app33DDBeyXEGRflo"
+    );
+    console.log('hello from rounds :>>')
+    base("Rounds").create(
+        rounds,
+        function (err, records) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            records.forEach(function (record) {
+                console.log(record.getId());
+            });
+        }
+    );
 }
 
 export const createOrder = async (orders: Order[]) => {
--- src/components/AddRounds.vue
+++ src/components/AddRounds.vue
@@ -1,24 +1,69 @@
 <template>
   <div>
-    <h1>Add an Order</h1>
+    <h1>Add Rounds</h1>
 
-    <div v-bind="record">
+    <div action="" style="display:flex align:center">
+      <ul>
+        <label>Name</label>
+        <br /><input v-model="round.Name" type="text" />
+        <br /><label>Grain</label>
+        <br /><input v-model="round.Grain" type="text" />
+        <br /><label>Muzzle Velocity</label>
+        <br /><input v-model="round['Muzzle Velocity']" type="text" />
+        <br /><label>Found</label>
+        <br /><input v-model="Found" type="text" />
+        <br /><label>Diameter</label>
+        <br /><input v-model="Diameter" type="text" />
+        <br /><label>Manufacturer</label>
+        <br /><input v-model="Manufacturer" type="text" />
+        <br />
+        <span>{{ round }}</span>
+        <br />
+        <button v-on:click="AddRounds">Submit</button>
+      </ul>
+    </div>
+
+    <!-- <div v-bind="record">
       <ul>
         <li>{{ Name }}</li>
         <li>{{ Id }}</li>
       </ul>
-    </div>
+    </div> -->
   </div>
 </template>
+<style scoped>
+label {
+  color: #a45;
+}
+input {
+  background: #a45;
+  color: limegreen;
+}
+button {
+  background: steelblue;
+  box-shadow: turquoise;
+}
+</style>
 
 <script>
 import { config } from "../config";
-import { createOrder } from "../../api/airtable";
+import { createOrder, createRound, Round } from "../../api/airtable";
 export default {
+  methods: {
+    async AddRounds() {
+      console.log(`this.round`, this.round);
+      console.log(`this.round.Name`, this.round.Name)
+      console.log(`this.round (keys)`, Object.keys( this.round))
+      // alert("added");
+      await createRound([{ Name: "Test ammo" }]);
+    },
+  },
   data() {
     return {
       customerName: "",
       config,
+      // round: Round = {},
+      round: {}
     };
   },
   async mounted() {
