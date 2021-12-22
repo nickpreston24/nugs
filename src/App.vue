b96f0301d0115f99a1709af1eae0ab5bf9319301
Michael Preston <michael.preston@cygrpdal.local>
2021-12-21 22:41:36 -0600

    + Dropped pants + Slid on ice

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link v-if="devmode" to="/">Home | </router-link>
-    <router-link v-if="!devmode" to="/about">About | </router-link>
-    <router-link v-if="!devmode" to="/signin">Sign In | </router-link>
-    <router-link to="/orders">Orders | </router-link>
-    <router-link to="/builds">Builds | </router-link>
-    <router-link to="/parts">Parts | </router-link>
-    <router-link v-if="devmode" to="/orders/add-part">To Ship | </router-link>
-    <router-link to="/sandbox"> Sandbox</router-link>
+  <div class="mt-10">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
   </div>
-  <router-view v-slot="{ Component }">
-    <transition name="route" mode="out-in">
-      <component :is="Component"></component>
-    </transition>
-  </router-view>
 </template>
+
 <script>
-import { devmode } from "./helpers/generators";
+import HelloWorld from "./components/HelloWorld.vue";
+
 export default {
-  data() {
-    return {
-      devmode: devmode(),
-    };
+  name: "App",
+  components: {
+    HelloWorld,
   },
 };
 </script>
-<style global>
-/* No hover borders */
-input:focus,
-select:focus,
-textarea:focus,
-button:focus {
-  outline: none;
-}
-
-/* Todo: move to tailwind component */
-label {
-  padding: 1rem;
-  font-weight: 500;
-  color: #42b983;
-}
-
-/* Todo: move to tailwind component */
-input.bullet-border {
-  border: 2px solid #42b983;
-  border-radius: 0% 50% 50% 0%;
-  outline: none;
-}
-
-/* Auto-center component and contents */
-.gentle-flex {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1ch;
-}
-
-#app {
-  font-family: "Trebuchet MS", Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  /* color: #2c3e50; */
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-/*route transitions*/
-.route-enter-from {
-  opacity: 0;
-  transform: translateX(100px);
-}
-
-.route-enter-active {
-  transition: all 0.15s ease-in-out;
-}
-
-.route-leave-to {
-  opacity: 0;
-  transform: translateX(-100px);
-}
-.route-leave-active {
-  transition: all 0.15s ease-in;
-}
-</style>
---
+++ vite.md
@@ -0,0 +1,31 @@
+Steps to install
+
+`npm init vite-app <project>`
+
+`cd <project>`
+
+`yarn`
+
+`yarn dev`
+
+`npm i -D tailwindcss@latest postcss@latest autoprefixer@latest`
+
+`npx tailwind init -p`
+
+Add the following to purge [] in `tailwindconfig.js`
+
+``` json    
+purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
+
+```
+Remove everything in index.css, replacing it with:
+
+``` css
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+```
+
+Sources:
+https://www.youtube.com/watch?v=ftT8nHHCZrI
+https://tailwindcss.com/docs/guides/vite
