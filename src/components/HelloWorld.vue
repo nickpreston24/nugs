07c5d129a523651cf1a3eb8c00b84f0055486c21
Michael Preston <michael.preston@cygrpdal.local>
2021-12-21 23:00:42 -0600

    exposing foo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,28 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div class="container">
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <div class="w-10 h-10 bg-pink rotate-90"></div>
+    <p>
+      Edit <code>components/HelloWorld.vue</code> to test hot module
+      replacement.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      count: 0
-    }
+      count: 0,
+    };
   },
   mounted() {
-    console.log(import.meta.env)
+    console.log("FOO? :>> ", import.meta.env.FOO);
   },
-}
+};
 </script>
