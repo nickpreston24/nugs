172b0276b445d4abf72949e8169892da14c6e6fb
Michael <michael.preston@mp2energy.com>
2022-01-28 03:21:37 -0600

    moved the server.js to _old so I can use serverless functions

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-module.exports = (req, res) => {
-    res.json({
+export default function handler(req, res) {
+    res.status(200).json({
         body: req.body,
         query: req.query,
         cookies: req.cookies,
     });
-};
+}
