8bd232edef28ad9d158e635796483975a6d7549a
mikepreston17 <michael.n.preston@gmail.com>
2022-02-16 15:30:36 -0600

    remved chakra ui and other non-essential pkgs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from "vue";
-import store from "./components/store";
 import App from "./App.vue";
-import Chakra, { CThemeProvider } from "@chakra-ui/vue";
 import router from "./router";
 import "./index.css";
 import "./assets/tailwind.css";
+import store from './components/store'
+import App from './App.vue'
+import router from './router'
 
-createApp(App)
-    .use(router)
-    .use(store)
-    .use(Chakra)
-    .mount("#app");
+
+createApp(App).use(router).use(store).mount('#app')
+
