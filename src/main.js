1e47cb62b1b2a9c03ccfac126d886c66f2caf40d
mikepreston17 <michael.n.preston@gmail.com>
2022-02-16 17:34:14 -0600

    Removed dups

--- src/main.js
+++ src/main.js
@@ -4,9 +4,5 @@ import router from "./router";
 import "./index.css";
 import "./assets/tailwind.css";
 import store from './components/store'
-import App from './App.vue'
-import router from './router'
-
 
 createApp(App).use(router).use(store).mount('#app')
-
