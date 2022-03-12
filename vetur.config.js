0667e80b0be9ba6ed167b87e526590093e8231af
mikepreston17 <michael.n.preston@gmail.com>
2022-03-12 17:54:21 -0600

    Fixed the Veture issue again

--- vetur.config.js
+++ vetur.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // Notice: It only affects the settings used by Vetur.
   settings: {
     "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": false
+    "vetur.experimental.templateInterpolationService": false,
   },
   // **optional** default: `[{ root: './' }]`
   // support monorepos
@@ -16,7 +16,7 @@ module.exports = {
       // **required**
       // Where is your project?
       // It is relative to `vetur.config.js`.
-      // root: '.',
+      root: ".",
       // **optional** default: `'package.json'`
       // Where is `package.json` in the project?
       // We use it to determine the version of vue.
@@ -34,9 +34,7 @@ module.exports = {
       // If you set it, you can get completion by that components.
       // It is relative to root property.
       // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
-      globalComponents: [
-        './src/components/**/*.vue'
-      ]
-    }
-  ]
-}
+      globalComponents: ["./src/components/**/*.vue"],
+    },
+  ],
+};
